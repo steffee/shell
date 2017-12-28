747a7ab10a43ad6471740d026dd0bdeb3032f433
steffee <34762025+steffee@users.noreply.github.com>
2017-12-28 18:28:55 +0800

    Update 0c.sh

--- 0c.sh
+++ 0c.sh
@@ -4,7 +4,7 @@ wget https://github.com/steffee/shell/blob/master/tcp_nanqinlang-1.3.1.sh && chm
 wget https://github.com/steffee/shell/blob/master/ssr.zip
 wget https://github.com/steffee/shell/blob/master/restartc.zip
 wget https://github.com/steffee/shell/blob/master/install_centos6.sh && chmod a+x install_centos6.sh
-wget 1c
+wget https://github.com/steffee/shell/blob/master/1c.sh
 unzip ssr.zip
 unzip restartc.zip && mv /root/restartc/* /root && chmod a+x /root/restart.sh && chmod a+x /root/r1.sh && chmod a+x /root/r2.sh
 bash tcp_nanqinlang-1.3.1.sh
