78de413261e3e40537eed41c92623f946139f1ea
steffee <34762025+steffee@users.noreply.github.com>
2017-12-28 18:37:31 +0800

    Update 0c.sh

--- 0c.sh
+++ 0c.sh
@@ -1,10 +1,10 @@
 echo -n "安装必要软件及下载bbr ssr“
 yum install -y wget unzip psmisc
-wget https://github.com/steffee/shell/blob/master/tcp_nanqinlang-1.3.1.sh && chmod a+x tcp_nanqinlang-1.3.1.sh
-wget https://github.com/steffee/shell/blob/master/ssr.zip
-wget https://github.com/steffee/shell/blob/master/restartc.zip
-wget https://github.com/steffee/shell/blob/master/install_centos6.sh && chmod a+x install_centos6.sh
-wget https://github.com/steffee/shell/blob/master/1c.sh
+wget --no-check-certificate https://github.com/steffee/shell/blob/master/tcp_nanqinlang-1.3.1.sh && chmod a+x tcp_nanqinlang-1.3.1.sh
+wget --no-check-certificate https://github.com/steffee/shell/blob/master/ssr.zip
+wget --no-check-certificate https://github.com/steffee/shell/blob/master/restartc.zip
+wget --no-check-certificate https://github.com/steffee/shell/blob/master/install_centos6.sh && chmod a+x install_centos6.sh
+wget --no-check-certificate https://github.com/steffee/shell/blob/master/1c.sh
 unzip ssr.zip
 unzip restartc.zip && mv /root/restartc/* /root && chmod a+x /root/restart.sh && chmod a+x /root/r1.sh && chmod a+x /root/r2.sh
 bash tcp_nanqinlang-1.3.1.sh
